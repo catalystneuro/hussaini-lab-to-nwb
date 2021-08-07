d23fcbeff64b4204b9b33dd1c93cf3f51b48204a
sbuergers <sbuergers@gmail.com>
2021-08-07 13:00:18 +0200

    add additional converters to nwb for various combinations of files

--- hussaini_lab_to_nwb/hussaininwbconverter.py
+++ hussaini_lab_to_nwb/hussaininwbconverter.py
@@ -1,10 +1,48 @@
 from nwb_conversion_tools import (
-    NWBConverter, AxonaRecordingExtractorInterface, AxonaPositionDataInterface
+    NWBConverter,
+    AxonaRecordingExtractorInterface,
+    AxonaUnitRecordingExtractorInterface,
+    AxonaPositionDataInterface,
+    AxonaLFPDataInterface
 )
 
 
-class HussainiNWBConverter(NWBConverter):
+# Make NWBConverters from datainterfaces for sensible combinations of datainterfaces
+class HussainiBinNWBConverter(NWBConverter):
+    data_interface_classes = dict(
+        AxonaRecordingExtractorInterface=AxonaRecordingExtractorInterface
+    )
+
+
+class HussainiBinPosLfpNWBConverter(NWBConverter):
     data_interface_classes = dict(
         AxonaRecordingExtractorInterface=AxonaRecordingExtractorInterface,
+        AxonaPositionDataInterface=AxonaPositionDataInterface,
+        AxonaLFPDataInterface=AxonaLFPDataInterface
+    )
+
+
+class HussainiTetrodeNWBConverter(NWBConverter):
+    data_interface_classes = dict(
+        AxonaUnitRecordingExtractorInterface=AxonaUnitRecordingExtractorInterface
+    )
+
+
+class HussainiPosNWBConverter(NWBConverter):
+    data_interface_classes = dict(
         AxonaPositionDataInterface=AxonaPositionDataInterface
+    )
+
+
+class HussainiLfpNWBConverter(NWBConverter):
+    data_interface_classes = dict(
+        AxonaLFPDataInterface=AxonaLFPDataInterface
+    )
+
+
+class HussainiUnitNWBConverter(NWBConverter):
+    data_interface_classes = dict(
+        AxonaUnitRecordingExtractorInterface=AxonaUnitRecordingExtractorInterface,
+        AxonaPositionDataInterface=AxonaPositionDataInterface,
+        AxonaLFPDataInterface=AxonaLFPDataInterface
     )
